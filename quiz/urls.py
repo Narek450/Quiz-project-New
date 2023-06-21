6fb99a0b604da1330af9aefbc6eadf99332f83fc
Narek <narekmanukyan450@gmail.com>
2023-06-21 14:14:06 -0700

    Your commit message

--- quiz/urls.py
+++ quiz/urls.py
@@ -1,12 +1,16 @@
 from . import views
 from django.urls import path
-from .views import Quiz, RandomQuestion, QuizQuestion, get_question_with_answers
+from .views import Quiz, RandomQuestion, QuizQuestion, get_question_with_answers, question_page, result_view
 
 app_name='quiz'
 
 urlpatterns = [
     path('', views.Quiz.as_view(), name='quiz'),
     path('question/<int:question_id>/', views.get_question_with_answers, name='question_with_answers'),
+    path('quiz/<int:quiz_id>/', question_page, name='question_page'),
+    path('quiz/result/', result_view, name='quiz_result'),
+    path('quiz/submit-answer/', views.submit_answer, name='submit-answer'),
+    path('submit-answer/', views.submit_answer, name='submit_answer'),
     path('r/<str:topic>', views.RandomQuestion.as_view(), name='random'),
     path('q/<str:topic>', views.QuizQuestion.as_view(), name='questions'),
 ]
--- quiz/views.py
+++ quiz/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from rest_framework.response import Response
-from .models import Quizzes, Question
+from .models import Quizzes, Question, Category, Answer
 from .serializers import QuizSerializer, QuestionSerializer, RandomQuestionSerializer
 from rest_framework.views import APIView
 
@@ -34,3 +34,44 @@ def get_question_with_answers(request, question_id):
         'answers': answers
     }
     return render(request, 'question_with_answers.html', context)
+
+def question_page(request, quiz_id):
+    quiz = Quizzes.objects.get(id=quiz_id)
+    questions = Question.objects.filter(quiz__id=quiz_id, quiz__category__id=6)
+
+    
+    context = {
+        'quiz': quiz,
+        'questions': questions,
+    }
+    return render(request, 'quiz/question_page.html', context)
+
+def submit_answer(request):
+    if request.method == 'POST':
+        score = calculate_score(request) 
+
+        context = {
+            'score': score,
+        }
+        return render(request, 'quiz/result.html', context)
+    else:
+        return render(request, 'quiz/answer_form.html')
+
+def calculate_score(request):
+    score = 0
+    for question in request.POST:
+        if question.startswith('question_'):
+            selected_answer_id = request.POST[question]
+            answer = Answer.objects.get(id=selected_answer_id)
+            if answer.is_right: 
+                score += 1
+    return score
+
+def result_view(request):
+    score = calculate_score(request)  
+
+    context = {
+        'score': score,
+    }
+
+    return render(request, 'quiz/result.html', context)
